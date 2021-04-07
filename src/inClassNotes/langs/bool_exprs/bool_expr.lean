c17430a3f6a99fc6daa0ca90b4be5367a2ac3e41
Jacob Rice <32640294+tyrant5678@users.noreply.github.com>
2021-04-06 22:22:23 -0400

    asd

--- src/inClassNotes/langs/bool_exprs/bool_expr.lean
+++ src/inClassNotes/langs/bool_exprs/bool_expr.lean
@@ -8,6 +8,8 @@ true && false,
 inductive bool_expr : Type
 | lit_expr : bool → bool_expr 
 | and_expr : bool_expr → bool_expr → bool_expr 
+| or_expr : bool_expr → bool_expr → bool_expr
+| not_expr : bool_expr → bool_expr
 
 open bool_expr 
 
@@ -20,7 +22,14 @@ def e3 := and_expr (lit_expr tt) [ff]
 def e4 := and_expr e3 [tt]
 
 notation e1 && e2 := and_expr e1 e2
+notation e1 || e2 := or_expr e1 e2
+notation !e1 := not_expr e1
 
+def en := ![tt]
+def tor := [tt] || [ff]
+def for := [ff] || [ff]
+def tnot := ![ff]
+def fnot := ![tt]
 def e3' := [tt] && [ff]
 def e4' := e3 && [tt]
 
@@ -31,5 +40,24 @@ def e4' := e3 && [tt]
 def bool_eval : bool_expr → bool
 | (lit_expr b) := b
 | (and_expr e1 e2) := band (bool_eval e1) (bool_eval e2)
+| (or_expr e1 e2) := bor (bool_eval e1) (bool_eval e2)
+| (not_expr e1) := bnot (bool_eval e1)
 
 #eval bool_eval e4'
+#eval bool_eval tor
+#eval bool_eval for
+#eval bool_eval tnot
+#eval bool_eval fnot
+
+example : ∀ (e1 e2 :bool_expr), (bool_eval (e1 && e2) = bool_eval (e2 && e1)) :=
+begin
+  assume e1 e2,
+  simp [bool_eval],
+  cases bool_eval e1,
+  cases bool_eval e2,
+  apply refl,
+  simp [band],
+  cases bool_eval e2,
+  simp [band],
+  apply refl,
+end
